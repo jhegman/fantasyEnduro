534abe3f8c1dbc5c334473d1bc6b38c02030cbc0
John Hegman <jhegman@Johns-MacBook-Pro.local>
2017-01-17 21:46:58 -0700

    Added a few stats to athlete pages

--- app/Http/Controllers/AthleteController.php
+++ app/Http/Controllers/AthleteController.php
@@ -17,7 +17,19 @@ class AthleteController extends Controller
 	//Make Athletes Clickable
 	public function show($id){
 		$athlete = Racer::findOrFail($id);
+		
+		$athleteData = Racer::find($id)->getRacersRace()->where('overall_place', 1)->get();
+		$racesWon = count($athleteData);
 
-		return view('show',compact('athlete'));
+		$count = 0;
+		for($i=0;$i<7;$i++){
+		$stageData = Racer::find($id)->getRacersRace()->where('stage_'. ($i+1) .'_place', 1)->get();
+		if(count($stageData) > 0){
+			$count++;
+		}
+		}
+		$stageWins = $count;
+		
+		return view('show',compact('athlete','racesWon','stageWins'));
 	}
 }
