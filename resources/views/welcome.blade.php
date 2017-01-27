eb5bfd656f40265db99f85b5df8f26aa874407c5
Peter Hegman <phegman@unionstmedia.com>
2017-01-26 23:55:36 -0500

    new landing page image. Run npm install

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,65 +7,11 @@
 
         <title>Fantasy Enduro</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="landing-bg-image flex-center position-ref full-height">
+            <div class="cover"></div><!-- /.cover -->
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
@@ -83,11 +29,15 @@
                 </div>
 
                 <div class="links">
-                    <a href="/athletes">Athletes</a>
-                    <a href="/set-lineup">Set Lineup</a>
-                    <a href="/upload-race">Upload Race</a>
-                    <a href="/upload-athlete">Upload Athletes</a>
-                    <a href="/results">Results</a>
+                    @if (Auth::check())
+                        <a href="/athletes">Athletes</a>
+                        <a href="/set-lineup">Set Lineup</a>
+                        <a href="/upload-race">Leagues</a>
+                        <a href="/results">Results</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
                 </div>
             </div>
         </div>
