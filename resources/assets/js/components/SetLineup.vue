c84944d20917303d41b49385d1ce0be8cafe144d
Peter Hegman <phegman@unionstmedia.com>
2017-01-30 19:46:08 -0500

    some ui updates. Styling out primary button a little and forms a little

--- resources/assets/js/components/SetLineup.vue
+++ resources/assets/js/components/SetLineup.vue
@@ -1,41 +1,58 @@
 <template>
     <div class="container">
-        <input v-model="searchString" placeholder="Search">
+        <div class="search-wrap lineup-search">
+            <i class="fa fa-search" aria-hidden="true"></i>
+            <input type="text" v-model="searchString" placeholder="Search">
+        </div><!-- /.search-wrap -->
         <transition name="fade">
             <div class="alert container set-linup-noty" :save-message="saveMessage" v-bind:class="[saveStatus ? 'alert-success' : 'alert-danger']" @click="closeNoty" v-if="showNoty" role="alert">{{saveMessage}}</div>
         </transition>
         <div class="row">
-            <div class="col-md-6">
-                <h2>Available Racers</h2>
-                <draggable :list="athletes" class="dragArea" :options="{group:'athletes'}">
-                <div class="racer" v-for="athlete in athletes" v-if="athlete.name.toLowerCase().indexOf(searchString.toLowerCase()) > -1" v-cloak>
-                    <header class="racer-header">
-                        <div class="head-shot" v-if="athlete.photo_url" :style="{backgroundImage: 'url(' + athlete.photo_url + ')'}"></div><!-- /.head-shot -->
-                        <div class="head-shot" v-else style="background-image: url('/img/placeholder.jpg')"></div><!-- /.head-shot -->
-                        <h4 class="racer-name">{{athlete.name}}</h2>
-                    </header>
-                    <div class="racer-stats">
-                        
-                    </div><!-- /.racer-stats -->
-                </div>
-                </draggable>
-            </div><!--./.col-md-6-->
-            <div class="col-md-6">
-                <h2>Your Lineup</h2>
-                <draggable :list="yourLineup" class="dragArea" :options="yourLineupOptions">
-                <div class="racer" v-for="athlete in yourLineup" v-cloak>
-                    <header class="racer-header">
-                        <div class="racer-stats">
+            <div class="col-md-6 available-racers">
+                <div class="content-wrap">
+                    <h2>Available Racers</h2>
+                    <draggable :list="athletes" class="dragArea" :options="{group:'athletes'}">
+                    <div class="racer" v-for="(athlete, index) in athletes" v-if="athlete.name.toLowerCase().indexOf(searchString.toLowerCase()) > -1" v-cloak>
+                        <header class="racer-header">
                             <div class="head-shot" v-if="athlete.photo_url" :style="{backgroundImage: 'url(' + athlete.photo_url + ')'}"></div><!-- /.head-shot -->
                             <div class="head-shot" v-else style="background-image: url('/img/placeholder.jpg')"></div><!-- /.head-shot -->
-                            <h4 class="racer-name">{{athlete.name}}</h2>
+                            <h4 class="racer-name">{{athlete.name}}</h4>
+                            <div class="plus-btn" @click="addAthleteToLineup(athlete, index)">
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                            </div><!-- /.plus-btn -->
+                        </header>
+                        <div class="racer-stats">
+                            
                         </div><!-- /.racer-stats -->
-                    </header>
-                </div>
-                </draggable>
+                    </div>
+                    </draggable>
+                </div><!-- /.content-wrap -->
+            </div><!--./.col-md-6-->
+            <div class="col-md-6 your-lineup">
+                <div class="content-wrap">
+                    <h2>Your Lineup</h2>
+                    <draggable :list="yourLineup" class="dragArea" :options="yourLineupOptions">
+                    <div class="racer" v-for="(athlete, index) in yourLineup" v-cloak>
+                        <header class="racer-header">
+                            <div class="head-shot" v-if="athlete.photo_url" :style="{backgroundImage: 'url(' + athlete.photo_url + ')'}"></div><!-- /.head-shot -->
+                            <div class="head-shot" v-else style="background-image: url('/img/placeholder.jpg')"></div><!-- /.head-shot -->
+                            <h4 class="racer-name">{{athlete.name}}</h4>
+                            <div class="minus-btn" @click="removeAthleteFromLineup(athlete, index)">
+                                <i class="fa fa-minus" aria-hidden="true"></i>
+                            </div><!-- /.plus-btn -->
+                        </header>
+                        <div class="racer-stats">
+                        </div><!-- /.racer-stats -->
+                    </div>
+                    </draggable>
+                </div><!-- /.content-wrap -->
             </div><!-- /.col-md-6 -->
         </div><!-- /.row -->
-        <button class="btn-primary" @click="onSave">Save Lineup</button>
+        <div class="row">
+            <div class="col-md-12 save-lineup">
+                <button class="btn-primary save-lineup-btn" @click="onSave">Save Lineup</button>
+            </div><!-- /.col-md-12 -->
+        </div><!-- /.row -->
     </div>
 </template>
 <script>
@@ -76,6 +93,14 @@
             closeNoty: function() {
                 this.showNoty = false;
             },
+            addAthleteToLineup: function(athlete, index) {
+                this.yourLineup.push(athlete);
+                this.athletes.splice(index, 1);
+            },
+            removeAthleteFromLineup: function(athlete, index) {
+                this.athletes.push(athlete);
+                this.yourLineup.splice(index, 1);
+            }
         },
         mounted: function () {
             if (window.location.pathname == '/set-lineup/men' || window.location.pathname == '/set-lineup/women') {
