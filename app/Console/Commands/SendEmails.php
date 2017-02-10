3ac0fff6b8b71411f3c5c7269ad377d2c50052fa
John Hegman <jhegman@Johns-MacBook-Pro.local>
2017-02-10 16:04:41 -0700

    Send email on specified dates. Haven't tested but should work

--- app/Console/Commands/SendEmails.php
+++ app/Console/Commands/SendEmails.php
@@ -5,6 +5,9 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Mail\SetLineup;
 use App\User;
+use Carbon\Carbon;
+use App\SelectionPeriod;
+use App\Lineup;
 
 class SendEmails extends Command
 {
@@ -39,9 +42,23 @@ class SendEmails extends Command
      */
     public function handle()
     {
+        //Check todays date
+        $send = false;
+        $today = Carbon::today();
+        $emailDates = SelectionPeriod::all();
+        //compare todays date to dates that emails should be sent
+        foreach ($emailDates as $emailDate) {
+            if(Carbon::parse($emailDate->send_email) == $today){
+                $send = true;
+            }
+        }
+
+        if($send == true){
         $users = User::all();
         foreach ($users as $key => $user) {
             \Mail::to($user)->send(new SetLineup);
         }
+    }
+
     }
 }
--- app/Http/Controllers/UploadAthleteController.php
+++ app/Http/Controllers/UploadAthleteController.php
@@ -122,6 +122,7 @@ class UploadAthleteController extends Controller
             $period->week = $time->week;
             $period->closed = $time->close;
             $period->reopen = $time->reopen;
+            $period->send_email = $time->send_email;
             $period->save();
         }
         return view('upload-tools.close-lineups');
