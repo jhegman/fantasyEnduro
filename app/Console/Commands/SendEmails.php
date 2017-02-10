71412844cd16536cc80a5d82b27da08c208bc8bd
John Hegman <jhegman@Johns-MacBook-Pro.local>
2017-02-10 12:57:19 -0700

    deleted migration in hope of fix

---
+++ app/Console/Commands/SendEmails.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Mail\SetLineup;
+use App\User;
+
+class SendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends email to users who have not set their lineup';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = User::all();
+        foreach ($users as $key => $user) {
+            \Mail::to($user)->send(new SetLineup);
+        }
+    }
+}
