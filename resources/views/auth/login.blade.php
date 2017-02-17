5e927809fa0a58ec421784983d90728b02ad88a7
Peter Hegman <phegman@unionstmedia.com>
2017-02-16 21:46:55 -0500

    styling out login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,31 +11,29 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                                <i class="fa fa-at" aria-hidden="true"></i>
+                                <label for="email" class="control-label">E-Mail Address</label>
+                            </div><!-- /.col-md-12 -->
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                                <i class="fa fa-lock" aria-hidden="true"></i>
+                                <label for="password" class="col-md-4 control-label">Password</label>
+                            </div><!-- /.col-md-12 -->
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group">
@@ -49,7 +47,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
