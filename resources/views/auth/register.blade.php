e7eaa223ed3dbb0925dc3b296b4cc1e20e3e87a9
Peter Hegman <phegman@unionstmedia.com>
2017-02-16 21:27:43 -0500

    styling out the register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container main-content">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
@@ -11,57 +11,54 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">UserName</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
+                            <div class="col-md-12">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" autofocus oninput="this.setAttribute('value', this.value);">
+                                <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                                <label for="name" class="control-label">UserName</label>
+                            </div><!-- /.col-md-12 -->
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" oninput="this.setAttribute('value', this.value);">
+                                <i class="fa fa-at" aria-hidden="true"></i>
+                                <label for="email" class="control-label">E-Mail Address</label>
+                            </div><!-- /.col-md-12 -->
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control" name="password" required oninput="this.setAttribute('value', this.value);" value="">
+                                <i class="fa fa-lock" aria-hidden="true"></i>
+                                <label for="password" class="control-label">Password</label>
+                            </div><!-- /.col-md-12 -->
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                            <div class="col-md-12">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required oninput="this.setAttribute('value', this.value);" value="">
+                                <i class="fa fa-lock" aria-hidden="true"></i>
+                                <label for="password-confirm" class="control-label">Confirm Password</label>
+                            </div><!-- /.col-md-12 -->
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary">
                                     Register
                                 </button>
