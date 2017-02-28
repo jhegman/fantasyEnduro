f04c82a34401926d82a34c7046aebe109d8a8acb
Peter Hegman <phegman@unionstmedia.com>
2017-02-27 21:56:11 -0500

    styling out user verification email

--- resources/views/user/user-verified.blade.php
+++ resources/views/user/user-verified.blade.php
@@ -5,12 +5,5 @@
     <div class="alert alert-success" role="alert">
         Account Verified! 
     </div>
-    <div class="links">
-        <a href="/athletes">Athletes</a>
-        <a href="/set-lineup">Set Lineup</a>
-        <a href="/leagues">Leagues</a>
-        <a href="/results">Results</a>
-        <a href="/rankings">Rankings</a>
-    </div>
 </div>
 @endsection
