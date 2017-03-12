75ba6814e0f768f8a72ee8d10bc55e3c015aa6d5
Peter Hegman <phegman@unionstmedia.com>
2017-03-11 19:34:07 -0500

    production mode vue

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ require('laravel-elixir-vue-2');
  |
  */
 
+
+
 elixir((mix) => {
     mix.sass('app.scss')
        .webpack('app.js')
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var webpack = require('webpack')
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
+  ]
+}
